7d19455c502222b7b3fffbaea35e0879142324d9
Auger89 <auger89@gmail.com>
2019-08-25 19:54:05 +0200

    Add Reach Router and create initial structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link, Router } from "@reach/router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <Login path="/login" />
+        <Signup path="/signup" />
+      </Router>
     </div>
   );
 }
